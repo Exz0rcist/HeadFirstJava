dcdacfc8851c7a8057fdc09271ee0a483d99bfe6
exz0rcist <exz0rcistb22@gmail.com>
2017-05-01 23:04:23 +0700

    Finished ocpypasting code from book =P

--- src/MiniMusicPlayer/MiniMusicPlayer2.java
+++ src/MiniMusicPlayer/MiniMusicPlayer2.java
@@ -15,10 +15,35 @@ public class MiniMusicPlayer2 implements ControllerEventListener{
 
             int[] eventsIWant = {127};
             sequencer.addControllerEventListener(this, eventsIWant);
-
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+            for (int i = 4; i < 60; i+= 4) {
+                track.add(makeEvent(176,1,127,0,i));
+                track.add(makeEvent(128,1,i,100,i + 2));
+            }
+            sequencer.setSequence(seq);
+            sequencer.setTempoInBPM(220);
+            sequencer.start();
         }
         catch (Exception ex){
             ex.printStackTrace();
         }
+    }
+
+    public MidiEvent makeEvent (int comd, int chan, int one, int two, int tick){
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        }
+        catch (Exception e){
+
+        }
+        return event;
+    }
+
+    public void controlChange(ShortMessage event){
+        System.out.println("lya!");
     }
 }
